b727ec831d196d34c2e14ddfb938826d91fc04f7
HARSHIT <harshitrajani1988@gmail.com>
2022-01-31 12:45:35 +0530

    Helperland

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Helperland.Data;
+using Helperland.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HelperlandContext _context;
+
+        public HomeController(ILogger<HomeController> logger,HelperlandContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Faq()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult Prices()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Visitor(ContactU o)
+        {
+            _context.ContactUs.Add(o);
+            _context.SaveChanges();
+            return View("ContactUs");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
